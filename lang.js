7e84ccde79088de9cad0cf6b1e306d1999a42f97
yves40 <yves.toubhans@free.fr>
2022-05-26 08:06:17 +0200

    Version

--- lang.js
+++ lang.js
@@ -17,7 +17,7 @@
     Place here all messages in all languages
 */
 const anylang = [
-    { "id": "u-copyright", "text": "Ratoon, 2.39 RC3 : May 24 2022"},
+    { "id": "u-copyright", "text": "Ratoon, 2.40 RC3 : May 26 2022"},
 ]
 
 const endict = [
