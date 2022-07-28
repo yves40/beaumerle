004bf00fa9c79d6a25b01baaaa917fe467e272e4
yves40 <yves.toubhans@free.fr>
2022-07-28 18:00:28 +0200

    Start work on admin

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@
   define('ROOT', dirname(__DIR__));
   define('IMAGEROOT', '/images/profile_pictures/');
   define('DEFAULTIMAGE', 'defaultuserpicture.png');
-  define('COPYRIGHT', '© Beaumerle by ratoon : Jul 28 2022, 1.18');
+  define('COPYRIGHT', '© Beaumerle by ratoon : Jul 28 2022, 1.19');
   define('APPTITLE', 'BoMerle-MVC');
 
   require_once __DIR__.'/../vendor/autoload.php';
@@ -30,10 +30,16 @@
   $app = new Application(dirname(__DIR__));
   
   // Register a class and a method for the controller to call it
+  
   $app->router->get('/', [SiteController::class, 'home']);
+
   $app->router->get('/knives/getall', [KnivesController::class, 'getAll']);
   $app->router->post('/knives/getbyid', [KnivesController::class, 'getKniveByID']);
   $app->router->post('/knives/getbylabel', [KnivesController::class, 'getKniveByLabel']);
+
+  $app->router->get('/admin/login', [AuthController::class, 'login']);
+  $app->router->post('/admin/login', [AuthController::class, 'login']);
+  
   
 
   $app->run();
