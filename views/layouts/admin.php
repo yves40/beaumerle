75b37624c4b471acf5d54fcad3bbf76c63b5674d
yves40 <yves.toubhans@free.fr>
2022-07-30 12:53:11 +0200

    Dive into bootstrap

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -18,10 +18,11 @@ use app\core\Application;
 
 <body>
   <!---------------------------- The menu section ---------------------------->
-  <nav class="navbar navbar-expand-lg bg-light">
+  <nav class="navbar navbar-expand-md bg-light">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <!-- Knives -->
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="a-knivesadmin" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <a id="a-knivesadmin" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
         </a>
         <ul class="dropdown-menu">
           <li><a id="a-kniveslist" class="dropdown-item" href="#"></a></li>
@@ -32,8 +33,9 @@ use app\core\Application;
           <li><a id="a-knivesedit" class="dropdown-item" href="#"></a></li>
         </ul>
       </li>
+      <!-- Users -->
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="a-usersadmin" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <a id="a-usersadmin" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
         </a>
         <ul class="dropdown-menu">
           <li><a id="a-userslist" class="dropdown-item" href="#"></a></li>
